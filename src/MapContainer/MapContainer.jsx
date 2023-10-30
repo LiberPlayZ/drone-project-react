aa3bf92a45c76e2a96a825a12275311d03f350ad
Daniel Liberman <dandosh5090@gmail.com>
2023-10-30 16:24:18 +0200

    adding israel polygon and draw polygon option

--- src/MapContainer/MapContainer.jsx
+++ src/MapContainer/MapContainer.jsx
@@ -1,24 +1,56 @@
 import React from 'react';
 import './MapContainer.css';
-import"leaflet/dist/leaflet.css";
+import "leaflet/dist/leaflet.css";
 import enviorment_variables from '../enviorment_variables';
-import {MapContainer,TileLayer} from 'react-leaflet'
+import { MapContainer, TileLayer, FeatureGroup, Polygon, Popup } from 'react-leaflet';
+import { EditControl } from 'react-leaflet-draw';
+import 'leaflet-draw/dist/leaflet.draw.css';
+import israelPolygon from './IsraelPolygonFile';
 // import L from 'leaflet'
 
-const MapContainer_component = ()=>{
+const MapContainer_component = () => {
     const center = enviorment_variables.Israel_cordinates;
     const zoom = enviorment_variables.Map_zoom;
     const url = enviorment_variables.TitleLayer_url;
     const attribution = enviorment_variables.TitleLayer_atribution;
+
+    const created = (e) => { // function to  save drawnn pokygon cordinates.
+        const p = e.layer._latlngs[0].map((latLng) => [latLng.lat, latLng.lng]);
+        console.log(p);
+    }
+
     return (
-       
-        <MapContainer center ={center} zoom ={zoom}>
+
+        <MapContainer center={center} zoom={zoom}>
+            <FeatureGroup>
+                <EditControl position='topright' onCreated={created}
+                    draw={{
+                        rectangle: false,
+                        circle: false,
+                        circlemarker: false,
+                        marker: false,
+                        polyline: false,
+
+                    }}>
+
+                </EditControl>
+            </FeatureGroup>
             <TileLayer
-            url={url}
-            attribution={attribution}
+                url={url}
+                attribution={attribution}
             />
+            <Polygon positions={israelPolygon.geometry.coordinates}>
+                <Popup>
+                    <div className='israel-popup'>
+                        <h2>{israelPolygon.properties.name}</h2>
+                        <p>Capital: {israelPolygon.properties.capital}</p>
+                        <p>Population: {israelPolygon.properties.population}</p>
+
+                    </div>
+                </Popup>
+            </Polygon>
         </MapContainer>
-     
+
     );
 }
 
